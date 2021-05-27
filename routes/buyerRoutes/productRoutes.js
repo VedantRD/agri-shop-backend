a0a036e02fa542c3f5d4f4d44d4c25cc36da10ac
VedantRD <vedant.debadwar@gmail.com>
2021-05-27 15:32:43 +0530

    changes

--- routes/buyerRoutes/productRoutes.js
+++ routes/buyerRoutes/productRoutes.js
@@ -25,4 +25,31 @@ router
             })
     })
 
+// search products
+router
+    .route('/products/search')
+    .get(async (req, res) => {
+        let { q, category } = req.query
+        if (category == 'All') {
+            category = ''
+        }
+        const namePattern = new RegExp('^.*' + q + '.*$', 'i')
+        const categoryPattern = new RegExp('^.*' + category + '.*$', 'i')
+
+        await Product
+            .find({ $and: [{ name: { $regex: namePattern } }, { category: { $regex: categoryPattern } }] }, { __v: 0 })
+            .populate('ownedBy', { password: 0, __v: 0 })
+            .then((products) => {
+                res.json({
+                    status: "success",
+                    message: "All products",
+                    length: products.length,
+                    products: products
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    })
+
 module.exports = router
